bbc2026323b607a49e7efea8a3b9afb056ff3c25
saehim <saehim@falinux.com>
2018-06-08 12:49:07 +0900

    project commit

---
+++ test-app/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2018. 6. 8.
+ *      Author: saehim
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+
+	return 0;
+}
+
